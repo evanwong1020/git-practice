f78ca9cf30cebf40960ac68195445694fd620a85
aliouas <78241615+aliouas@users.noreply.github.com>
2023-09-19 12:56:26 -0400

    Update comp491.py

--- comp491.py
+++ comp491.py
@@ -11,6 +11,7 @@ def print_names():
              'Melantha Chen'
              'Myra Hasan'
              'Belgin Koç', 
+             'Souha Alioua',
              ]
     print('Here are COMP491 members in alphabetical order:')
     sorted_names = sorted(names)
